d994ff88c6a8ee7d85f4832e09554ef8dee4e416
dosoid <dos333@seznam.cz>
2024-12-13 15:38:06 +0100

    3 ukoly hotove

--- Lekce 3.py
+++ Lekce 3.py
@@ -1,3 +1,36 @@
 # Lekce 3
 
+# Ukol 1
+
+# user_1 = dict()
+
+# user_1["name"] = "Marek"
+# user_1["surname"] = "Parek"
+
+# user_email = {"email": "marek.parek@gmail.com"}
+# user_1.update(user_email)
+# print(f"User #01: {user_1}")
+
+# Ukol 2
+
+# jmeno = 'Marek'
+# heslo = '1234'
+# uzivatel = {'Marek': '1234'}
+# if uzivatel.get(jmeno) == heslo:
+#     print(f"Ahoj {jmeno}, vítej v aplikaci! Pokračuj ...")
+# else:
+#     print("Uživatelské jméno nebo heslo nejsou v pořádku!")
+
+# Ukol 3
+
+# cisla_1 = (1, 1, 2, 3, 4)
+# cisla_2 = (5, 6, 7, 7, 8)
+
+# set_1 = set(cisla_1)
+# set_2 = set(cisla_2)
+# sjednocene_hodnoty = set_1.union(set_2)
+# print(f"Sjednocené hodnoty ze zadání: {sjednocene_hodnoty}")
+
+# Ukol 4
+
 
