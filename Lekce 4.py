8157543d674c211e7be324004661650a1f8a6e1b
dosoid <dos333@seznam.cz>
2024-12-15 13:53:07 +0100

    Ukol 4.1 hotov

---
+++ Lekce 4.py
@@ -0,0 +1,19 @@
+# Ukol 1
+# veta = 'Zvuk řeči je produkován poměrně otevřenou konfigurací vokálního traktu'
+# samohlasky = 'aeiouáéíóú'
+# souhlasky = 'bcčdďfghjklmnňprřsštťvzžcdž'
+# vysledek = dict()
+# pocet_samohlasky = int()
+# pocet_souhlasky = int()
+
+# for pismeno in veta:
+#     if pismeno.lower() in samohlasky:
+#         pocet_samohlasky += 1
+#     elif pismeno.lower() in souhlasky:
+#         pocet_souhlasky += 1
+
+# vysledek[samohlasky] = pocet_samohlasky
+# vysledek[souhlasky] = pocet_souhlasky
+
+# print(f"Počet souhlasek: {vysledek[souhlasky]} | Počet samohlasek: {vysledek[samohlasky]}")
+
